1e1973ec0b6bc5fc07c175d8e23e943da46b62f5
Your Name <trungduongnguyen2004@gmail.com>
2023-12-30 16:41:22 -0800

    change the word

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Hello Three.js',
+            'Donut',
             {
                 font: font,
                 size: 0.5,
